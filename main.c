e5a53b93c96b9bc0dfd11fe49904949e8a7a0fba
UselessWiz <joshuaharrison111222@gmail.com>
2025-10-20 16:54:59 +1100

    Get arg-processing up to date

--- main.c
+++ main.c
@@ -14,27 +14,27 @@
 #define MAXFILELEN 256
 #define MAXMESSAGELEN 256
 
-/* Takes an input file (.bmp image), an output file name to create and the message the user wishes to compress 
+/* Takes an input file (.bmp image), an output file name to create and the message the user wishes to compress
 and encode into the image. Outputs a status based on if the image was successfully encoded or not. (Khanh) */
 int encode(char infile[], char outfile[], char message[]);
 
-/* Takes an input .bmp file and places the decoded message into the outstring char array. 
+/* Takes an input .bmp file and places the decoded message into the outstring char array.
 Outputs a status based on if the image was successfully encoded or not. (Khanh) */
 int decode(char infile[], char outstring[]);
 
-/* Simple check to see if the provided filename ends in ".bmp\0". If it does, all good, if not warn the 
-user that there might be errors and ask if they want to proceed. Can also check first two bytes for 
+/* Simple check to see if the provided filename ends in ".bmp\0". If it does, all good, if not warn the
+user that there might be errors and ask if they want to proceed. Can also check first two bytes for
 0x42 0x4D as per format standard. Only prompt in interactive mode */
 int checkFileType(char filename[]);
 
-/* Takes a string in and returns a compressed version of it - most likely with RLE (Sam)*/ 
-char* compressMessage(char message[]);
+/* Takes a string in and returns a compressed version of it - most likely with RLE (Sam)*/
+char *compressMessage(char message[]);
 
 /* Takes a compressed string in and returns the decompressed version of it (Sam) */
-char* decompressMessage(char compressed[]);
+char *decompressMessage(char compressed[]);
 
 /* Using the queue structure, get recently accessed files in order */
-char** getRecentFiles(queue_t* queue);
+char **getRecentFiles(queue_t *q);
 
 void printMenu(void);
 
--- stegano.c
+++ stegano.c
@@ -1,10 +1,78 @@
 #include "stegano.h"
+#include <stdio.h>
 
-#define MAXQUEUESIZE 5
-
-struct queue
+void initialiseQueue(queue_t *q)
 {
-    char** items;
-    int front;
-    int back;
-};
+    q->front = 0;
+    q->back = 0;
+    q->count = 0;
+}
+
+int isEmpty(queue_t *q)
+{
+    if (q->count == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(queue_t *q)
+{
+    if (q->count == MAX_SIZE)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void enqueue(queue_t *q, char value)
+{
+    if (isFull(q))
+    {
+        printf("Queue is full\n");
+        return;
+    }
+    q->items[q->back] = value;
+    q->back = (q->back + 1) % MAX_SIZE;
+    q->count++;
+}
+
+void dequeue(queue_t *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    q->front = (q->front + 1) % MAX_SIZE;
+    q->count--;
+}
+
+char peek(queue_t *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty\n");
+        return -1;
+    }
+    return q->items[q->front];
+}
+
+void printQueue(queue_t *q)
+{
+    if (isEmpty(q))
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    printf("Current Queue: ");
+    int i, current = q->front;
+    for (i = 0; i < q->count; i++)
+    {
+        printf("%c ", q->items[current]);
+        current = (current + 1) % MAX_SIZE;
+    }
+    printf("\n");
+}
